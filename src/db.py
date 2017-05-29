a9db29ef872594d46b31a53e46571ec53cc9ba36
James Shi <jshi@live.ca>
2017-05-28 20:28:28 -0700

    moved src to it its own dir

---
+++ src/db.py
@@ -0,0 +1,86 @@
+import sqlite3
+
+class DB:
+    def __init__(self):
+        self.connection = sqlite3.connect('feed.db')
+        self.cursor = self.connection.cursor()
+
+    def reset(self):
+        self.cursor.executescript(
+            '''
+            DROP TABLE IF EXISTS subscriber;
+            CREATE TABLE subscriber (
+                user varchar(50) not null,
+                feed varchar(50) not null,
+                UNIQUE (user, feed)
+            );
+
+            DROP TABLE IF EXISTS subscription;
+            CREATE TABLE subscription (
+                feed varchar(50) not null,
+                article varchar(50) not null,
+                UNIQUE (feed, article)
+            );
+            '''
+        )
+        self.connection.commit()
+
+    def subscribe(self, user, feed):
+        try:
+            self.cursor.execute(
+                '''
+                INSERT INTO subscriber (user, feed)
+                VALUES (?, ?)
+                ''', (user, feed)
+            )
+            self.connection.commit()
+        except sqlite3.IntegrityError:
+            return False
+
+        return True
+
+    def unsubscribe(self, user, feed):
+        self.cursor.execute(
+            '''
+            DELETE FROM subscriber
+            WHERE user=? and feed=?
+            ''', (user, feed)
+        )
+        self.connection.commit()
+
+    def add_article_to_feed(self, article, feed):
+        try:
+            self.cursor.execute(
+                '''
+                INSERT INTO subscription (feed, article)
+                VALUES (?, ?)
+                ''' , (feed, article)
+            )
+            self.connection.commit()
+        except sqlite3.IntegrityError:
+            return False
+
+        return True
+
+    def get_feeds_by_subscriber(self, user):
+        self.cursor.execute(
+            '''
+            SELECT feed FROM subscriber
+            WHERE user=?
+            ''', (user, )
+        )
+        self.connection.commit()
+        return [feed for feed, in self.cursor.fetchall()]
+
+    def get_articles_by_subscriber(self, user):
+        self.cursor.execute(
+            '''
+            SELECT article FROM subscription
+            WHERE feed IN (
+                SELECT feed FROM subscriber
+                WHERE user=? 
+            )
+            ''', (user, )
+        )
+        self.connection.commit()
+        return [article for article, in self.cursor.fetchall()]
---
+++ src/feed.py
@@ -0,0 +1,67 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+from requests import codes
+
+import logging
+import sys
+
+from db import DB
+
+app = Flask(__name__)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
+
+db = DB()
+
+@app.route('/reset', methods=['POST'])
+def reset():
+    app.logger.info('reseting db')
+    db.reset()
+    return 'Successfully reset database'
+
+@app.route('/subscribe', methods=['POST'])
+def subscribe():
+    user = request.form['user']
+    feed = request.form['feed']
+    app.logger.info('{} is subscribing to {}'.format(user, feed))
+    if not db.subscribe(user, feed):
+        return '{} already subscribed to {}'.format(user, feed), codes.conflict
+
+    return jsonify({'user': user, 'feed': feed}), codes.created
+
+@app.route('/unsubscribe', methods=['DELETE'])
+def unsubscribe():
+    user = request.form['user']
+    feed = request.form['feed']
+    app.logger.info('{} is unsubscribing from {}'.format(user, feed))
+    db.unsubscribe(user, feed)
+    return '', codes.no_content
+
+@app.route('/publish', methods=['POST'])
+def publish():
+    feed = request.form['feed']
+    article = request.form['article']
+    app.logger.info('Adding {} to {}'.format(article, feed))
+    if not db.add_article_to_feed(article, feed):
+        return '{} already in {}'.format(article, feed), codes.conflict
+
+    return jsonify({'feed': feed, 'article': article}), codes.created
+
+@app.route('/feeds')
+def feeds():
+    user = request.args.get('user')
+    app.logger.info('Requesting feeds for {}'.format(user))
+    feeds = db.get_feeds_by_subscriber(user)
+    return jsonify(feeds), codes.ok
+
+@app.route('/articles')
+def articles():
+    user = request.args.get('user')
+    app.logger.info('Requesting articles for {}'.format(user))
+    articles = db.get_articles_by_subscriber(user)
+    return jsonify(articles), codes.ok
+
+if __name__ == "__main__":
+    app.run()
